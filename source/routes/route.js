709867f1eb7e4e15ffacd79cdf66ce6ec4642392
Leonardo Camp <leofrederiche@gmail.com>
2022-06-12 22:41:52 -0300

    Change route name

--- source/routes/route.js
+++ source/routes/route.js
@@ -5,11 +5,11 @@ const nameRoute = require("./name")
 const route = (app) => {
     app.route("/").get(index)
 
-    app.route("/data").get(nameRoute.index)
-    app.route("/data").post(nameRoute.insert)
-    app.route("/data/:index").put(nameRoute.update)
-    app.route("/data/:index").get(nameRoute.show)
-    app.route("/data/:index").delete(nameRoute.destroy)
+    app.route("/name").get(nameRoute.index)
+    app.route("/name").post(nameRoute.insert)
+    app.route("/name/:index").put(nameRoute.update)
+    app.route("/name/:index").get(nameRoute.show)
+    app.route("/name/:index").delete(nameRoute.destroy)
 }
 
 module.exports = { route }
